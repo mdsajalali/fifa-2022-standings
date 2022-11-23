aeae6e867d362e859b1c4239ad5e301787da0572
mdsajalali <itzsajal.me@gmail.com>
2022-11-23 13:03:57 +0600

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,28 @@
-import './globals.css'
+import Link from "next/link";
+import React from "react";
+import { DateTime } from "luxon";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body className="container max-w-6xl mx-auto flex flex-col min-h-screen px-5">
+        <nav className="py-3 border-b-2">
+          <div className="flex justify-between">
+            <Link href="/">
+              <h1 className="text-3xl font-black text-red-600">FIFA WC 2022</h1>
+            </Link>
+          </div>
+        </nav>
+        <main className="my-4 grow">{children}</main>
+        <footer className="border-t-2 py-4 ">
+          <p className="flex justify-center space-x-4">
+            <span>&copy; {DateTime.now().year}</span>
+            <span>Only for Educational purpose ⚽</span>
+          </p>
+        </footer>
+      </body>
     </html>
-  )
+  );
 }
---
+++ app/standings/page.jsx
@@ -0,0 +1,29 @@
+import { DateTime } from "luxon";
+import React from "react";
+import StandingSection from "./standing";
+
+export default async function StangingPage() {
+  const response = await fetch("http://api.cup2022.ir/api/v1/standings", {
+    headers: {
+      authorization: process.env.API_KEY ?? "",
+    },
+    next: {
+      revalidate: 10,
+    },
+  });
+
+  const standings = (await response.json()).data;
+
+  return (
+    <div>
+      <p className="font-semibold text-sm mb-4 text-end text-gray-700 italic">
+        Last updated: {DateTime.now().toLocaleString(DateTime.DATETIME_MED)}
+      </p>
+      <div className="grid md:grid-cols-2  gap-4 ">
+        {standings.map((standing) => (
+          <StandingSection standing={standing} key={standing._id} />
+        ))}
+      </div>
+    </div>
+  );
+}
